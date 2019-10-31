e6ffd075231dff157e8a7df2c94f1a7f61a2c4ce
Fayaazuddin Ali Mohammad <fayaazuddinalimohammad@FayaazuddinsMBP.lan>
2019-10-31 20:12:57 +0100

    more questions to Java_Cert

---
+++ src/main/java/Java_cert/Question8.java
@@ -0,0 +1,39 @@
+package Java_cert;
+
+class Question8 {
+    Question8() {
+        System.out.println("Shape");
+    }
+
+    protected void area() {
+        System.out.println("Shape");
+    }
+}
+
+class Square extends Question8 {
+    int side;
+    Square(){
+        /*insert Code Here*/
+        this.side = side;
+    }
+
+    public void area() {
+        System.out.println("Shape");
+    }
+}
+
+class Rectangle extends Square {
+    int len, br;
+
+    Rectangle(int x, int y) {
+        /*Insert Code here*/
+        super();
+        super.side=x;
+        len = x;
+        br = y;
+    }
+
+    public void area() {
+        System.out.println("Rectangle");
+    }
+}
