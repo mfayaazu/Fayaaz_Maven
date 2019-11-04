5340823f7b96ff19e4c20cdace57021b593c3cdb
Fayaaz Ali <fayaazuddin.ali.mohammad@wirelesscar.com>
2019-11-04 16:02:49 +0100

    Java cert commits

--- src/main/java/Java_cert/Question13.java
+++ src/main/java/Java_cert/Question13.java
@@ -5,21 +5,23 @@ import java.io.IOException;
 
 public class Question13 {
 
-  public void recDelete (String dirName) throws IOException {
+  public void recDelete(String dirName) throws IOException {
     File[] listOfFiles = new File(dirName).listFiles();
-    if (listOfFiles != null && listOfFiles.length >0) {
+    if (listOfFiles != null && listOfFiles.length > 0) {
       for (File aFile : listOfFiles) {
-      if (aFile.isDirectory()) {
-        recDelete (aFile.getAbsolutePath ());
-      } else {
-        if (aFile.getName ().endsWith (".class")) aFile.delete ();
+        if (aFile.isDirectory()) {
+          recDelete(aFile.getAbsolutePath());
+        } else {
+          if (aFile.getName().endsWith(".class")) {
+            aFile.delete();
+          }
+        }
       }
-    }
     }
   }
 
   public static void main(String[] args) throws IOException {
-    Question13 object1 =new Question13();
+    Question13 object1 = new Question13();
     object1.recDelete("C:\\Cert_Check");
   }
 }
