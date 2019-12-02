dafce3a8f09e07c6dbb82aba85892280be28ccd2
Fayaaz Ali <fayaazuddin.ali.mohammad@wirelesscar.com>
2019-12-02 16:13:00 +0100

    Java cert Commits

---
+++ src/main/java/Java_cert/Question98.java
@@ -0,0 +1,19 @@
+package Java_cert;
+
+public class Question98 {
+
+  public static void main(String[] args) {
+    String str1 = "Java";
+    String[] str2 = {"J", "a", "v", "a"};
+    String str3 = "";
+    for (String str : str2) {
+      str3 = str3 + str;
+    }
+    boolean b1 = (str1 == str3);
+    boolean b2 = (str1.equals(str3));
+    System.out.println(b1 + ", " + b2);
+  }
+}
+
+//output: false, true
+//Correct answer will be A
