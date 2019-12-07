d8da7dcf612b1b1ca27b2124c40655c35708f26a
Fayaazuddin Ali Mohammad <fayaazuddinalimohammad@Fayaazuddins-MacBook-Pro.local>
2019-12-07 18:21:58 +0100

    Java cert examples

--- src/main/java/Java_cert/Question86.java
+++ src/main/java/Java_cert/Question86.java
@@ -1,35 +1,38 @@
-//package Java_cert;
-//
-//public class Question86 {
-//
-//    class Vehicle{
-//        int x;
-//        Vehicle(){
-//            this(10);       //line n1
-//        }
-//        Vehicle(int x){
-//            this.x =x;
-//
-//        }
-//    }
-//    static class Car extends Vehicle{
-//        int y;
-//        Car(){
-//            //super();
-//            this(20);       //line n2
-//        }
-//        Car(int y){
-//            this.y=y;
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return super.x + ":" + this.y;
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        Vehicle y = new Car();
-//        System.out.println(y);
-//    }
-//}
+package Java_cert;
+
+public class Question86 {
+
+    static class Vehicle{
+        int x;
+        Vehicle(){
+            this(10);       //line n1
+        }
+        Vehicle(int x){
+            this.x =x;
+        }
+    }
+    static class Car extends Vehicle {
+        int y;
+        Car(){
+            //super();
+            this(20);       //line n2
+        }
+        Car(int y){
+            this.y=y;
+        }
+
+        @Override
+        public String toString() {
+            return super.x + ":" + this.y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Vehicle y = new Car();
+        System.out.println(y);
+    }
+}
+
+
+//Output is :  10:20
+// Correct answer is B
