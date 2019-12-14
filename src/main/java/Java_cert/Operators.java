7f93c33afd7f87aa90b80e69628c82e42e6fe4b0
Fayaazuddin Ali Mohammad <fayaazuddinalimohammad@FayaazuddinsMBP.lan>
2019-12-14 16:27:01 +0100

    Fayaaz Did This change to help other team member

---
+++ src/main/java/Java_cert/Operators.java
@@ -0,0 +1,23 @@
+package Java_cert;
+
+public class Operators {
+
+
+    public static void main(String[] args) {
+        int i =3;
+
+
+        i++;
+        ++i;
+        ++i;
+        i++;
+        ++i;
+
+        System.out.println(i++);
+        System.out.println(i);
+    }
+}
+
+
+//++i--> first increment then execute
+//i++--> first execute then increment
---
+++ src/main/java/Object/Operators.java
@@ -0,0 +1,13 @@
+package Object;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        int y= 2;
+        for (int i = 1; i <= 10; i++) {
+            long z = y*i;
+            System.out.println(y + "X" + i + "= " + z);
+        }
+        System.out.println(y);
+    }
+}
